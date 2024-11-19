33cdf7f39922ef6aae8dd644b0e163dab3577c59
renji18 <aadarshjha1401@gmail.com>
2024-11-19 16:37:37 +0530

    merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import React from "react";
 
 export const metadata: Metadata = {
   title: "PIT Portal",
